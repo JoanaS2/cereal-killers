2914d9641e2d54f487aa867da68f8b1cf28ff2f0
Joana Silva <jslvag@outlook.com>
2024-04-14 15:51:20 +0100

    Create progress bar

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score')
+const progressBarFull = document.getElementById('progressBarFull')
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -139,7 +140,12 @@ getNewQuestion = () => {
   }
   questionCounter++;
   // questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  //Update the progress bar
+  // console.log((questionCounter / MAX_QUESTIONS) * 100);
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
