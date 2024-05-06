c57827bfa8db69d9bbc886733dc90b7c2e23d4fc
Joana Silva <jslvag@outlook.com>
2024-05-06 14:31:43 +0100

    Add game draft

--- game.js
+++ game.js
@@ -4,24 +4,34 @@ const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById('score')
 const progressBarFull = document.getElementById('progressBarFull')
 
+const imageContainer = document.getElementById("imageContainer");
+const imageOverlay = document.getElementById("imageOverlay");
+let isImageBlurred = false; //Variable to track the blurred state of the image
+
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-
-
 const CORRECT_BONUS = 5;
 const MAX_QUESTIONS = 12;
 
-let questions = [{
+// event listener to toggle blue effect 
+imageOverlay.addEventListener("click", () => {
+  isImageBlurred = !isImageBlurred;
+  imageContainer.classList.toggle("blurred");
+});
+
+const questions = [{
+    /* Your array of questions */
     question: "What is the largest planet in the solar system?",
     choice1: "Jupiter",
     choice2: "Neptune",
     choice3: "Saturn",
     choice4: "Uranus",
-    answer: 1
+    answer: 1,
+    image: "/assets/images/jupiter-1 .jpg"
   },
   {
     question: "Which of the following is the largest island in the world?",
@@ -29,7 +39,8 @@ let questions = [{
     choice2: "Madagascar",
     choice3: "Borneo",
     choice4: "Greenland",
-    answer: 4
+    answer: 4,
+    image: "/assets/images/greenland (1).jpg"
 
   },
   {
@@ -38,15 +49,17 @@ let questions = [{
     choice2: "Pepsi",
     choice3: "Coca Cola",
     choice4: "Dr Pepper",
-    answer: 3
+    answer: 3,
+    image: "/assets/images/cola.webp"
   },
   {
-    question: "What year was the famous movie Titanic released?",
-    choice1: "1997",
-    choice2: "1995",
-    choice3: "2000",
-    choice4: "1998",
-    answer: 1
+    question: "Which country produces the most coffee beans in the world?",
+    choice1: "Brazil",
+    choice2: "Switzerland",
+    choice3: "Vietnam",
+    choice4: "Peru",
+    answer: 1,
+    image: "/assets/images/coffee-beans.jpg"
 
   },
   {
@@ -54,8 +67,9 @@ let questions = [{
     choice1: "7",
     choice2: "5",
     choice3: "1",
-    choice4: "3",
-    answer: 4
+    choice4: "0",
+    answer: 4,
+    image: "/assets/images/octo (1).jpg"
   },
   {
     question: "Which planet is nearest to the sun?",
@@ -63,7 +77,8 @@ let questions = [{
     choice2: "Mercury",
     choice3: "Jupiter",
     choice4: "Venus",
-    answer: 2
+    answer: 2,
+    image: "/assets/images/mercury-1.jpg"
   },
   {
     question: "How many bones does a shark have?",
@@ -71,7 +86,8 @@ let questions = [{
     choice2: "50",
     choice3: "100",
     choice4: "150",
-    answer: 1
+    answer: 1,
+    image: "/assets/images/sharkie.jpg"
   },
   {
     question: "What is the largest continent?",
@@ -79,15 +95,17 @@ let questions = [{
     choice2: "Africa",
     choice3: "Asia",
     choice4: "North America",
-    answer: 3
+    answer: 3,
+    image: "/assets/images/asia-map.jpeg"
   },
   {
-    question: "In which country did the Olympics originate?",
-    choice1: "Italy",
-    choice2: "Greece",
-    choice3: "Egypt",
-    choice4: "China",
-    answer: 2
+    question: "What is the most populous city in the world?",
+    choice1: "New York",
+    choice2: "Tokyo",
+    choice3: "Dhaka",
+    choice4: "Shanghai",
+    answer: 2,
+    image: "/assets/images/tokyo.jpg"
   },
   {
     question: "What year did Disneyland inaugurate?",
@@ -95,7 +113,8 @@ let questions = [{
     choice2: "1980",
     choice3: "1970",
     choice4: "1955",
-    answer: 4
+    answer: 4,
+    image: "/assets/images/disneyland.jpeg"
   },
   {
     question: "Where was the mojito cocktail created?",
@@ -103,7 +122,8 @@ let questions = [{
     choice2: "Mexico",
     choice3: "Cuba",
     choice4: "Peru",
-    answer: 3
+    answer: 3,
+    image: "/assets/images/mojitos (1).jpg"
   },
   {
     question: "What is the tallest building in the world?",
@@ -111,9 +131,11 @@ let questions = [{
     choice2: "Burj Khalifa",
     choice3: "Shanghai Tower",
     choice4: "Taipepi 101",
-    answer: 2
+    answer: 2,
+    image: "/assets/images/burj-khalif.jpg"
   },
 ];
+
 
 // Shuffle questions
 function shuffle(array) {
@@ -123,33 +145,31 @@ function shuffle(array) {
   }
 }
 
-startGame = () => {
+function startGame() {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
   shuffle(availableQuestions);
-  // console.log(availableQuestions);
   getNewQuestion();
-};
+}
 
-getNewQuestion = () => {
-
-  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    //go to the end page
+function getNewQuestion() {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     return window.location.assign("/end.html");
   }
   questionCounter++;
-  // questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-
-  //Update the progress bar
-  // console.log((questionCounter / MAX_QUESTIONS) * 100);
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
+
+  // Add blur effect to image
+  imageContainer.classList.add("blurred");
+
+  // const imageContainer = document.querySelector(".image-container");
+  imageContainer.innerHTML = `<img src="${currentQuestion.image}" alt="Question Image">`;
 
   choices.forEach(choice => {
     const number = choice.dataset["number"];
@@ -157,46 +177,48 @@ getNewQuestion = () => {
   });
 
   availableQuestions.splice(questionIndex, 1);
-  // console.log(availableQuestions);
   acceptingAnswers = true;
-};
+}
 
 choices.forEach(choice => {
   choice.addEventListener('click', e => {
-    // console.log(e.target);
+    if (isImageBlurred) return;
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    // console.log('Selected Answer:', selectedAnswer);
-    // console.log('Correct Answer:', currentQuestion.answer);
-
-
-    // let classToApply = 'incorrect';
-    // if (selectedAnswer === currentQuestion.answer) {
-    //   classToApply = 'correct';
-    // }
-
     const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
+    } else {
+      // Show the correct answer
+      const correctChoice = choices.find(choice => choice.dataset["number"] == currentQuestion.answer);
+      correctChoice.parentElement.classList.add('correct');
+
+      selectedChoice.parentElement.classList.add('incorrect');
     }
 
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
+      // Remove highlighting of the correct answer
+      const correctChoice = choices.find(choice => choice.dataset["number"] == currentQuestion.answer);
+      correctChoice.parentElement.classList.remove('correct');
       getNewQuestion();
+      // Remove blur effect when the user selects an answer
+      imageContainer.classList.remove("blurred");
     }, 1000)
   });
 });
 
-incrementScore = num => {
+
+function incrementScore(num) {
   score += num;
   scoreText.innerText = score;
-};
+}
 
 startGame();
